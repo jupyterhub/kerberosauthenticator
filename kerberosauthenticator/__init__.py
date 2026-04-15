2ae5fea2b19550723511ceceebf83d6e5fe13152
Simon Li <orpheus+devel@gmail.com>
2026-04-15 22:05:24 +0100

    Replace versioneer with setuptools-scm
    If we want automated versioning setuptools-scm has the advantage of automatically tracking files so we can get rid of MANIFEST.in

--- kerberosauthenticator/__init__.py
+++ kerberosauthenticator/__init__.py
@@ -1,5 +1,3 @@
 from .auth import KerberosAuthenticator, KerberosLocalAuthenticator
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from ._version import __version__
