5d2609f51b31cd180707d42172a95c9316b96000
Simon Li <orpheus+devel@gmail.com>
2026-04-28 22:52:48 +0100

    Remove _version.py from git

--- kerberosauthenticator/__init__.py
+++ kerberosauthenticator/__init__.py
@@ -1,5 +1,9 @@
 from .auth import KerberosAuthenticator, KerberosLocalAuthenticator
-from ._version import __version__
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "UNKNOWN"
 
 __all__ = [
   "__version__",
